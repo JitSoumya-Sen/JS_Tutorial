1ee63d28c53364af4a237f7a6bf87c4be7040a3a
JitSoumya-Sen <150611804+JitSoumya-Sen@users.noreply.github.com>
2023-11-22 07:06:33 +0000

    Info upgrade

--- 01_basics/Comparison.js
+++ 01_basics/Comparison.js
@@ -1,12 +1,13 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+/*
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);
+console.log("02" > 1);
 
 console.log(null > 0);
 console.log(null == 0);
@@ -15,7 +16,18 @@ console.log(null >= 0); // Only Comparison operators treats null as zero then co
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
-
+*/
 // === 
 
 console.log("2" === 2);
+console.log("2" == 2);
+console.log("true" == true);
+
+/* In the case of == operator
+
+If either operand is a string, the other operand will be converted to a string.
+If either operand is a number, the other operand will be converted to a number.
+If either operand is a boolean, it will be converted to a number (true becomes 1 and false becomes 0).
+If one operand is an object and the other is a primitive value, the object will be converted to a primitive value before the comparison is made.
+If one of the operands is null or undefined, the other must also be null or undefined to return true. Otherwise it will return false.
+*/

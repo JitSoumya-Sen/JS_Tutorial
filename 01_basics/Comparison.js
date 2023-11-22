2b86ff600b74a9865a921f5b7e5bd7e11bc95121
JitSoumya-Sen <150611804+JitSoumya-Sen@users.noreply.github.com>
2023-11-22 06:25:22 +0000

    comparison

---
+++ 01_basics/Comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // Only Comparison operators treats null as zero then compare it
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);

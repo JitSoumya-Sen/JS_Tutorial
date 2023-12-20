488126f3702ddc9de3e9a9114a6d7b58299e6420
JitSoumya-Sen <150611804+JitSoumya-Sen@users.noreply.github.com>
2023-12-20 06:20:26 +0000

    date&time

--- 01_basics/dates.js
+++ 01_basics/dates.js
@@ -3,4 +3,40 @@ console.log(date.toString())
 console.log(date.toLocaleString())
 
 const anydate = new Date(2023, 0, 27)
+const anotherdate = new Date(2023, 0, 27, 5, 3)
+let mycreatedate = new Date("01-20-2023")
 console.log(anydate.toString());
+console.log(anotherdate.toLocaleString());
+console.log(mycreatedate.toLocaleString());
+
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})

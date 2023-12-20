37a7a6832f8108b194794cf316f5df34d4eeceba
JitSoumya-Sen <150611804+JitSoumya-Sen@users.noreply.github.com>
2023-12-20 08:51:42 +0000

    Updated

--- 02_basics/object.js
+++ 02_basics/object.js
@@ -10,13 +10,14 @@ const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
     [mySym]: "mykey1",
+    mySym:"mykey1", //different mySym
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
-
+console.log(JsUser);
 // console.log(JsUser.email)
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])

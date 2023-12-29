6a1cae7f9bf9d6fca824d87800d0d9a0f952f5d1
JitSoumya-Sen <150611804+JitSoumya-Sen@users.noreply.github.com>
2023-12-29 07:06:57 +0000

    avoiding name conflict

--- 04_iterations/filter_map_reduce.js
+++ 04_iterations/filter_map_reduce.js
@@ -10,13 +10,13 @@
 
 const mynNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {
+// const newNums = mynNums.filter( (num) => {
 //     return num > 4
 // } )
 
 // const newNums = []
 
-// myNums.forEach( (num) => {
+// mynNums.forEach( (num) => {
 //     if (num > 4) {
 //         newNums.push(num)
 //     }
